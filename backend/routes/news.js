8e46782eff9daa3bcaff2d80e89dbea53ea8bd53
Sachin P. <spaithane9172@gmail.com>
2025-04-20 00:59:35 +0530

    some api changes

--- backend/routes/news.js
+++ backend/routes/news.js
@@ -1,37 +1,18 @@
 import express from "express";
-import fetch from "node-fetch";
-
 const router = express.Router();
+import { REACT_APP_NEWS_API_KEY } from "../config.js";
 
 router.get("/", async (req, res) => {
   const { category = "general", page = 1, pageSize = 10 } = req.query;
-  const apiKey = "85967f37713f44c18381204922365bb9";
-  const url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${apiKey}&page=${page}&pageSize=${pageSize}`;
+  const apiKey = process.env.REACT_APP_NEWS_API_KEY || REACT_APP_NEWS_API_KEY;
+  const url = `https://newsdata.io/api/1/latest?apikey=pub_815081c01955d76e6bea77e4d9fb9fa37c2f1`;
 
   try {
-    const response = await fetch(url, {
-      headers: {
-        "User-Agent": "Mozilla/5.0",
-        Referer: "https://newsapi.org",
-      },
-    });
-
+    const response = await fetch(url);
     const data = await response.json();
-
-    if (!response.ok) {
-      console.error("NewsAPI Error:", data);
-      return res
-        .status(response.status)
-        .json({ error: data.message || "NewsAPI error" });
-    }
-
     res.json(data);
   } catch (error) {
-    console.error("Fetch Failed:", error);
-    res
-      .status(500)
-      .json({ error: "Internal Server Error", message: error.message });
+    res.status(500).json({ error: "Failed to fetch news" });
   }
 });
-
 export default router;
